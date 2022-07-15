a9f3e9b846c27fb03594179aea944f8ba260e6dd
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-07-15 21:47:28 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+    
+function createItem(array) {
+    return galleryItems.reduce((acc, item) => acc + `<li clas="gallery__item"> <a class="gallery__link" href="large-image.jpg"> <img class="gallery__image" src = "${item.preview}" data-source ="${item.original}" alt = "${item.description}"> </a> </li>`, "");
+}
+gallery.insertAdjacentHTML("afterbegin", createItem(galleryItems))
+debugger
+gallery.addEventListener('click', onModalOpen);
+
