92236ff09abd8f3da0c9f4b77bb89f5c41b7a4ea
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-07-16 15:14:04 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,16 +2,13 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const gallery = document.querySelector('.gallery');
-
   
 function createItem(array) {
     return galleryItems.reduce((acc, item) => acc + `<li clas="gallery__item"> <a class="gallery__link" href="${item.original}"> <img class="gallery__image" src = "${item.preview}" data-source ="${item.original}" alt = "${item.description}"> </a> </li>`, "");
 };
 gallery.insertAdjacentHTML("afterbegin", createItem(galleryItems));
 
-
 gallery.addEventListener('click', onModalOpen);
-
 
 function onModalOpen(event) {
     event.preventDefault();
@@ -19,13 +16,11 @@ function onModalOpen(event) {
     modal.show();
     window.addEventListener("keydown", onModalClose);
     function onModalClose(evt) {
-    if (evt.code === 'Escape') {
-        modal.close();
-        console.log();
+        if (evt.code === 'Escape') {
+            modal.close();
+        }
+        window.removeEventListener("keydown", onModalClose)
     }
-    window.removeEventListener("keydown", onModalClose)
-}
-
 }
 
 
