293996317e22222fad41f7b98bdf0e136823640b
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-07-18 00:07:10 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+
+  
+function createItem(array) {
+    return galleryItems.reduce((acc, item) => acc + `<a class="gallery__item" href="${item.original}"> <img class="gallery__image" src = "${item.preview}" alt = "${item.description}"> </a>`, "");
+};
+gallery.insertAdjacentHTML("afterbegin", createItem(galleryItems));
+
+gallery.addEventListener('click', onModalOpen);
+
+function onModalOpen(event) {
+    event.preventDefault();
+    const lightbox = new SimpleLightbox('.gallery a');
+    console.log(lightbox)
+    lightbox.show();
+    lightbox.addEventListener('click', onModalClose)
+    function onModalClose(evt) {
+        lightbox.destroy()
+        console.log('close')
+    }
+}
